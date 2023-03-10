5efd849194eab149b45e8f007c7bddc523d0ce95
tony <tony@DESKTOP-HH929ON>
2023-03-09 20:49:02 -0800

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,219 @@
+#include "Image.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+MyImage			inImage, outImage;
+HINSTANCE		hInst;
+TCHAR szTitle[MAX_LOADSTRING];
+TCHAR szWindowClass[MAX_LOADSTRING];
+
+// Foward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+void 				ColorPixel(char* imgBuf, int w, int h, int x, int y);
+
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// Read in a parameter from the command line
+	stringstream stream(lpCmdLine);
+	int size;
+	int codeNum;
+	string filePath;
+	stream >> filePath >> size >> codeNum;
+	// Create a separate console window to display output to stdout
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+	cout << "file: " << filePath << endl << "codebook size: " << codeNum << endl;
+
+	// Set up the input images
+	int w = 352;
+	int h = 288;
+	inImage.setWidth(w);
+	inImage.setHeight(h);
+	inImage.setImagePath(filePath.c_str());
+	inImage.ReadImage();
+
+	// Set up the output img
+	outImage = inImage;
+
+	// Initialize 
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_IMAGE, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+	if (!InitInstance (hInstance, nCmdShow)) 
+	{
+		return FALSE;
+	}
+	hAccelTable = LoadAccelerators(hInstance, (LPCTSTR)IDC_IMAGE);
+	while (GetMessage(&msg, NULL, 0, 0)) 
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) 
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return msg.wParam;
+}
+
+void ColorPixel(char* imgBuf, int w, int h, int x, int y)
+{
+	imgBuf[(3 * y * w) +  3 * x] = 0;
+	imgBuf[(3 * y * w) +  3 * x + 1] = 0;
+	imgBuf[(3 * y * w) +  3 * x + 2] = 0;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX); 
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= (WNDPROC)WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, (LPCTSTR)IDI_IMAGE);
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= (LPCSTR)IDC_IMAGE;
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, (LPCTSTR)IDI_SMALL);
+
+	return RegisterClassEx(&wcex);
+}
+
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance;
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+   
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+	TCHAR szHello[MAX_LOADSTRING];
+	LoadString(hInst, IDS_HELLO, szHello, MAX_LOADSTRING);
+	RECT rt;
+	GetClientRect(hWnd, &rt);
+
+	switch (message) 
+	{
+		case WM_COMMAND:
+			wmId    = LOWORD(wParam); 
+			wmEvent = HIWORD(wParam); 
+			switch (wmId)
+			{
+				case IDM_ABOUT:
+				   DialogBox(hInst, (LPCTSTR)IDD_ABOUTBOX, hWnd, (DLGPROC)About);
+				   break;
+				case ID_MODIFY_IMAGE:
+				   outImage.Modify();
+				   InvalidateRect(hWnd, &rt, false); 
+				   break;
+				case IDM_EXIT:
+				   DestroyWindow(hWnd);
+				   break;
+				default:
+				   return DefWindowProc(hWnd, message, wParam, lParam);
+			}
+			break;
+		case WM_PAINT:
+			{
+				hdc = BeginPaint(hWnd, &ps);
+				// TO DO: Add any drawing code here...
+				char text[1000];
+				strcpy(text, "Original image (Left) and image after compression (Right)\n");
+				DrawText(hdc, text, strlen(text), &rt, DT_LEFT);
+
+				BITMAPINFO bmi;
+				CBitmap bitmap;
+				memset(&bmi,0,sizeof(bmi));
+				bmi.bmiHeader.biSize = sizeof(bmi.bmiHeader);
+				bmi.bmiHeader.biWidth = inImage.getWidth();
+				bmi.bmiHeader.biHeight = -inImage.getHeight();  // Use negative height.  DIB is top-down.
+				bmi.bmiHeader.biPlanes = 1;
+				bmi.bmiHeader.biBitCount = 24;
+				bmi.bmiHeader.biCompression = BI_RGB;
+				bmi.bmiHeader.biSizeImage = inImage.getWidth()*inImage.getHeight();
+
+				SetDIBitsToDevice(hdc,
+								  10,50,inImage.getWidth(),inImage.getHeight(),
+								  0,0,0,inImage.getHeight(),
+								  inImage.getImageData(),&bmi,DIB_RGB_COLORS);
+
+				SetDIBitsToDevice(hdc,
+								  outImage.getWidth()+50,50,outImage.getWidth(),outImage.getHeight(),
+								  0,0,0,outImage.getHeight(),
+								  outImage.getImageData(),&bmi,DIB_RGB_COLORS);
+
+
+				EndPaint(hWnd, &ps);
+			}
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+   }
+   return 0;
+}
+
+
+
+
+// Mesage handler for about box.
+LRESULT CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_INITDIALOG:
+				return TRUE;
+
+		case WM_COMMAND:
+			if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) 
+			{
+				EndDialog(hDlg, LOWORD(wParam));
+				return TRUE;
+			}
+			break;
+	}
+    return FALSE;
+}
+
+
